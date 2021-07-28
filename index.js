d40d76992febebe89cf9f4e6d00f257c0d2944a3
marceloMAJR <marcelo.martinsajr@gmail.com>
2021-07-28 06:00:03 -0300

    #1

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 const express = require("express");
 const app = express();
+const connection = require("./database/database");
+
+//View engine
+app.set('view engine', 'ejs')
+
+
+//Static
+app.use(express.static('public'));
+
+//body parser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Database
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso!");
+    }).catch((error) => {
+        console.log(error);
+    });
+
+app.get("/", (req,res) => {
+    res.render("index");
+});
+
+app.listen(8080, () => {
+    console.log("O sevidor está rodando")
+});
