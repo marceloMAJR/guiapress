5a45db87073c230838a7d7aabcb31354b78c2539
marceloMAJR <marcelo.martinsajr@gmail.com>
2021-07-30 06:10:55 -0300

    #2

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const connection = require("./database/database");
 
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
+const Article = require("./articles/Article");
+const Category = require("./categories/Category");
+
 //View engine
 app.set('view engine', 'ejs')
 
@@ -22,6 +28,12 @@ connection
     }).catch((error) => {
         console.log(error);
     });
+
+
+
+app.use("/",categoriesController);
+app.use("/",articlesController);
+
 
 app.get("/", (req,res) => {
     res.render("index");
